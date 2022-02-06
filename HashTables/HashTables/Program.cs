9d5bb0b7068a8b3bd80eee39aaff8333541e37ac
akshata2005 <akshatasawant1805@gmail.com>
2022-02-06 20:07:31 +0530

    [akshata] Add:Finding frequency of words

---
+++ HashTables/HashTables/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTables
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Welcome to Hashmap.");
+            string paragraph = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+            ///Split converts paragraph  into array of sub strings.
+            string[] para = paragraph.Split(' ');
+            ///Creating reference of MyMapNode.
+            MyMapNode<int, string> hash = new MyMapNode<int, string>(para.Length);
+            int key = 0;
+            ///foreach iterates on paragraph and adds key and value to hash.
+            foreach (string word in para)
+            {
+                hash.Add(key, word);
+                key++;
+            }
+            Console.WriteLine("Frequency :" + hash.GetFrequency("paranoid"));
+
+        }
+    }
+}
